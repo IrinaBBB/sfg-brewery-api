0df678a9bea2c60f2147f5d5b4d1dbfec1b1b2f2
IrinaB <balkarovairina@gmail.com>
2026-05-25 07:55:47 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/sfgbreweryapi/SfgBreweryApiApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgbreweryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgBreweryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgBreweryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgbreweryapi/SfgBreweryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgbreweryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgBreweryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
